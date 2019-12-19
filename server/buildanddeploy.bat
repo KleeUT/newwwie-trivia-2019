c8af48b3492f2d1a2e40b096932ff257f1222a00
kleeut <klee.ut@gmail.com>
2019-12-19 21:52:15 +1100

    :art:refactoring to bring the package.json to the root directory

--- server/buildanddeploy.bat
+++ server/buildanddeploy.bat
@@ -11,4 +11,4 @@ call sam build
 echo ----------build done ------------
 call sam package --output-template-file packaged.yaml --s3-bucket kleeut-lambda
 echo -----------package done ---------------
-call sam deploy --template-file .\packaged.yaml --region ap-southeast-2 --capabilities CAPABILITY_IAM --stack-name sam-socket-test
+call sam deploy --template-file .\packaged.yaml --region ap-southeast-2 --capabilities CAPABILITY_IAM --stack-name sam-socket-test-directory-change
