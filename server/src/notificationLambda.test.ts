f6a22273f7ac3a02fefab173fe3b3bd49367c5e7
kleeut <klee.ut@gmail.com>
2020-01-01 17:01:26 +1100

    :art: ran prettier

--- server/src/notificationLambda.test.ts
+++ server/src/notificationLambda.test.ts
@@ -1,6 +1,6 @@
-import { DynamoDBStreamEvent } from 'aws-lambda'
-import { handler as notificationLambdaHandler } from './notificationLambda'
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDBStreamEvent } from "aws-lambda";
+import { handler as notificationLambdaHandler } from "./notificationLambda";
+import { DynamoDB } from "aws-sdk";
 
 describe(notificationLambdaHandler, () => {
   it("Should exit quickly on an insert event", () => {
@@ -11,8 +11,8 @@ describe(notificationLambdaHandler, () => {
   });
   it("Should fetch matching question record when current question key updated", async () => {
     (DynamoDB as jest.Mock<any>).mockImplementation(() => {
-      return
-    })
+      return;
+    });
     await notificationLambdaHandler(updateEvent, {
       callbackWaitsForEmptyEventLoop: false,
       functionName: "name",
@@ -21,13 +21,13 @@ describe(notificationLambdaHandler, () => {
       memoryLimitInMB: 1,
       logGroupName: "",
       awsRequestId: "",
-      done: () => { },
-      fail: () => { },
-      succeed: () => { },
+      done: () => {},
+      fail: () => {},
+      succeed: () => {},
       logStreamName: "",
       getRemainingTimeInMillis: () => 0
-    })
-    expect(DynamoDB)
+    });
+    expect(DynamoDB);
     expect(false).toBe(true);
   });
   it("Should exit notify with question data when current question key updated", () => {
@@ -36,74 +36,76 @@ describe(notificationLambdaHandler, () => {
 });
 
 export const updateEvent: DynamoDBStreamEvent = {
-  "Records": [
+  Records: [
     {
-      "eventID": "cb39508f58cb16325f901f2ddc2e5a07",
-      "eventName": "MODIFY",
-      "eventVersion": "1.1",
-      "eventSource": "aws:dynamodb",
-      "awsRegion": "ap-southeast-2",
-      "dynamodb": {
-        "ApproximateCreationDateTime": 1575754141,
-        "Keys": {
-          "kid": {
-            "S": "currentQuestion"
+      eventID: "cb39508f58cb16325f901f2ddc2e5a07",
+      eventName: "MODIFY",
+      eventVersion: "1.1",
+      eventSource: "aws:dynamodb",
+      awsRegion: "ap-southeast-2",
+      dynamodb: {
+        ApproximateCreationDateTime: 1575754141,
+        Keys: {
+          kid: {
+            S: "currentQuestion"
           },
-          "sk": {
-            "S": "-"
+          sk: {
+            S: "-"
           }
         },
-        "NewImage": {
-          "question": {
-            "S": "1"
+        NewImage: {
+          question: {
+            S: "1"
           },
-          "round": {
-            "S": "0"
+          round: {
+            S: "0"
           },
-          "break": {
-            "BOOL": true
+          break: {
+            BOOL: true
           },
-          "kid": {
-            "S": "currentQuestion"
+          kid: {
+            S: "currentQuestion"
           },
-          "sk": {
-            "S": "-"
+          sk: {
+            S: "-"
           }
         },
-        "SequenceNumber": "2235100000000008730164444",
-        "SizeBytes": 63,
-        "StreamViewType": "NEW_IMAGE"
+        SequenceNumber: "2235100000000008730164444",
+        SizeBytes: 63,
+        StreamViewType: "NEW_IMAGE"
       },
-      "eventSourceARN": "arn:aws:dynamodb:ap-southeast-2:624538342145:table/sam-socket-test-TriviaTable-1BSEFRY1LLJP0/stream/2019-12-07T08:27:17.385"
+      eventSourceARN:
+        "arn:aws:dynamodb:ap-southeast-2:624538342145:table/sam-socket-test-TriviaTable-1BSEFRY1LLJP0/stream/2019-12-07T08:27:17.385"
     }
   ]
-}
+};
 
 const insertEvent: DynamoDBStreamEvent = {
-  "Records": [
+  Records: [
     {
-      "eventID": "d68b423012d5b3bce24751421a43e772",
-      "eventName": "INSERT",
-      "eventVersion": "1.1",
-      "eventSource": "aws:dynamodb",
-      "awsRegion": "ap-southeast-2",
-      "dynamodb": {
-        "ApproximateCreationDateTime": 1575369045,
-        "Keys": {
-          "kid": {
-            "S": "connection:EH3k_c_zSwMCGqw="
+      eventID: "d68b423012d5b3bce24751421a43e772",
+      eventName: "INSERT",
+      eventVersion: "1.1",
+      eventSource: "aws:dynamodb",
+      awsRegion: "ap-southeast-2",
+      dynamodb: {
+        ApproximateCreationDateTime: 1575369045,
+        Keys: {
+          kid: {
+            S: "connection:EH3k_c_zSwMCGqw="
           }
         },
-        "NewImage": {
-          "kid": {
-            "S": "connection:EH3k_c_zSwMCGqw="
+        NewImage: {
+          kid: {
+            S: "connection:EH3k_c_zSwMCGqw="
           }
         },
-        "SequenceNumber": "3950500000000007790329561",
-        "SizeBytes": 60,
-        "StreamViewType": "NEW_IMAGE"
+        SequenceNumber: "3950500000000007790329561",
+        SizeBytes: 60,
+        StreamViewType: "NEW_IMAGE"
       },
-      "eventSourceARN": "arn:aws:dynamodb:ap-southeast-2:624538342145:table/sam-socket-test-TriviaTable-1379EFINODOOZ/stream/2019-12-02T10:32:59.139"
+      eventSourceARN:
+        "arn:aws:dynamodb:ap-southeast-2:624538342145:table/sam-socket-test-TriviaTable-1379EFINODOOZ/stream/2019-12-02T10:32:59.139"
     }
   ]
-}
+};
--- server/src/notificationLambda.ts
+++ server/src/notificationLambda.ts
@@ -1,7 +1,7 @@
-import { DynamoDBStreamEvent, Context, DynamoDBRecord } from 'aws-lambda'
-import fetch from 'node-fetch';
-import { ApiGatewayManagementApi, DynamoDB } from 'aws-sdk'
-import { connect } from 'http2';
+import { DynamoDBStreamEvent, Context, DynamoDBRecord } from "aws-lambda";
+import fetch from "node-fetch";
+import { ApiGatewayManagementApi, DynamoDB } from "aws-sdk";
+import { connect } from "http2";
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 
@@ -10,61 +10,75 @@ import { connect } from 'http2';
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
-const apiId = "cydrqavlhh"
-const region = 'ap-southeast-2';
+const apiId = "cydrqavlhh";
+const region = "ap-southeast-2";
 const stage = "deploytest";
 const currentQuestionKey = "currentQuestion";
 const connectionPrefix = "connection:";
-const connectionKey = 'connection'
+const connectionKey = "connection";
 const endpointApi = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}`;
 
 interface QuestionDynamoImage {
-  "question": {
-    "S": string
-  },
-  "round": {
-    "S": string
-  },
-  "break": {
-    "BOOL": boolean
-  },
-  "kid": {
-    "S": string
-  },
-  "sk": {
-    "S": string
-  }
+  question: {
+    S: string;
+  };
+  round: {
+    S: string;
+  };
+  break: {
+    BOOL: boolean;
+  };
+  kid: {
+    S: string;
+  };
+  sk: {
+    S: string;
+  };
 }
 
-interface UpdatedQuestion { round: Number, question: Number, break: Boolean }
+interface UpdatedQuestion {
+  round: Number;
+  question: Number;
+  break: Boolean;
+}
 
-const isCurrentQuestionDynamoKey = (record: DynamoDBRecord): boolean => !!(record.dynamodb &&
-  record.dynamodb.Keys &&
-  record.dynamodb.Keys.kid &&
-  record.dynamodb.Keys.kid.S &&
-  record.dynamodb.Keys.kid.S === currentQuestionKey)
+const isCurrentQuestionDynamoKey = (record: DynamoDBRecord): boolean =>
+  !!(
+    record.dynamodb &&
+    record.dynamodb.Keys &&
+    record.dynamodb.Keys.kid &&
+    record.dynamodb.Keys.kid.S &&
+    record.dynamodb.Keys.kid.S === currentQuestionKey
+  );
 
 const isItAQuestionUpdateEvent = (event: DynamoDBStreamEvent): boolean =>
-  event.Records.some(record => record.eventName === "MODIFY" &&
-    isCurrentQuestionDynamoKey
-  )
+  event.Records.some(
+    record => record.eventName === "MODIFY" && isCurrentQuestionDynamoKey
+  );
 
-const getNewQuestionInfoFrom = (event: DynamoDBStreamEvent): UpdatedQuestion => {
-  const questionUpdateRecord = <QuestionDynamoImage><unknown>(((event.Records.find(isCurrentQuestionDynamoKey) || {}).dynamodb || {}).NewImage || {});
+const getNewQuestionInfoFrom = (
+  event: DynamoDBStreamEvent
+): UpdatedQuestion => {
+  const questionUpdateRecord = <QuestionDynamoImage>(
+    (<unknown>(
+      (((event.Records.find(isCurrentQuestionDynamoKey) || {}).dynamodb || {})
+        .NewImage || {})
+    ))
+  );
 
   return {
     round: Number(questionUpdateRecord.round.S),
     question: Number(questionUpdateRecord.question.S),
     break: questionUpdateRecord.break.BOOL
-  }
-}
+  };
+};
 
 export const handler = async (event: DynamoDBStreamEvent, context: Context) => {
   try {
@@ -78,11 +92,9 @@ export const handler = async (event: DynamoDBStreamEvent, context: Context) => {
     // console.log("New Question:", newQuestion)
 
     const manApi = new ApiGatewayManagementApi({
-      apiVersion: '2018-11-29',
+      apiVersion: "2018-11-29",
       endpoint: endpointApi
     });
-
-
 
     const userFacingQuetion = await userFacingQuetionPromise;
     // console.log("userFacingQuetion", userFacingQuetion);
@@ -90,116 +102,126 @@ export const handler = async (event: DynamoDBStreamEvent, context: Context) => {
     const existingConnections = await activeConnectionsPromise;
     // console.log('connections,', existingConnections)
 
-
-
-
     const sendPromises = existingConnections.map(connection => {
       const params: ApiGatewayManagementApi.PostToConnectionRequest = {
         ConnectionId: connection || "",
         Data: JSON.stringify(userFacingQuetion)
-      }
-      console.log(`attempting to send welcome message to ${connection}, with api url ${endpointApi}`)
-      return manApi.postToConnection(params).promise()
-        .then((response) => console.log(`done sending to ${connection}`, response))
-        .catch((err) => {
-          console.log(`Couldnt send to connection ${connection}`, err)
+      };
+      console.log(
+        `attempting to send welcome message to ${connection}, with api url ${endpointApi}`
+      );
+      return manApi
+        .postToConnection(params)
+        .promise()
+        .then(response =>
+          console.log(`done sending to ${connection}`, response)
+        )
+        .catch(err => {
+          console.log(`Couldnt send to connection ${connection}`, err);
           removeConnectionOnFailure(connection);
         });
-    })
-    console.log(`Waiting for ${existingConnections.length} functions to finish`)
+    });
+    console.log(
+      `Waiting for ${existingConnections.length} functions to finish`
+    );
     await Promise.all(sendPromises);
-    console.log("Done")
-    return "HAPPY DAYS"
+    console.log("Done");
+    return "HAPPY DAYS";
   } catch (err) {
-    console.error("Something went wrong", err)
+    console.error("Something went wrong", err);
     return err;
   }
-
 };
 
 interface UserFacingQuetion {
-  title: String
-  body: String
-  break: Boolean
+  title: String;
+  body: String;
+  break: Boolean;
 }
 
-const removeConnectionOnFailure = async (connectionId: string): Promise<void> => {
+const removeConnectionOnFailure = async (
+  connectionId: string
+): Promise<void> => {
   try {
     const TableName = process.env.DYNAMO_TABLE || "";
     const db = new DynamoDB({
-      region: 'ap-southeast-2'
+      region: "ap-southeast-2"
     });
-    await db.deleteItem({
-      TableName,
-      Key: {
-        'kid': { S: 'connection' },
-        'sk': { S: `connection:${connectionId}` },
-      }
-    }).promise()
-    console.log(`Removed connection ${connectionId}`)
+    await db
+      .deleteItem({
+        TableName,
+        Key: {
+          kid: { S: "connection" },
+          sk: { S: `connection:${connectionId}` }
+        }
+      })
+      .promise();
+    console.log(`Removed connection ${connectionId}`);
     // const ret = await axios(url);
   } catch (err) {
     console.log(err);
     return err;
   }
-}
+};
 
-const getQuestion = async (question: UpdatedQuestion): Promise<UserFacingQuetion> => {
+const getQuestion = async (
+  question: UpdatedQuestion
+): Promise<UserFacingQuetion> => {
   if (question.break) {
     return {
       title: "Break",
       body: "",
       break: question.break
-    }
+    };
   }
   const TableName = process.env.DYNAMO_TABLE || "";
   const db = new DynamoDB({
     region
   });
-  const item = await db.getItem({
-    TableName,
-    Key: {
-      kid: { S: "question" },
-      sk: { S: `r${question.round}:q${question.question}` }
-    }
-  }).promise()
+  const item = await db
+    .getItem({
+      TableName,
+      Key: {
+        kid: { S: "question" },
+        sk: { S: `r${question.round}:q${question.question}` }
+      }
+    })
+    .promise();
   console.log("Question Item", item);
 
   return {
     title: `Round: ${question.round} Question: ${question.question}`,
     body: (item.Item || {}).markdown.S || "",
     break: false
-  }
-}
+  };
+};
 
 const allActiveConnections = async (): Promise<string[]> => {
   const TableName = process.env.DYNAMO_TABLE || "";
   const db = new DynamoDB({
     region
   });
-  const dynamoResponse = await db.query({
-    TableName,
-    KeyConditions: {
-      kid: {
-        ComparisonOperator: "EQ",
-        AttributeValueList: [
-          { S: connectionKey }
-        ]
-      },
-      sk: {
-        ComparisonOperator: "BEGINS_WITH",
-        AttributeValueList: [
-          { S: connectionPrefix }
-        ]
+  const dynamoResponse = await db
+    .query({
+      TableName,
+      KeyConditions: {
+        kid: {
+          ComparisonOperator: "EQ",
+          AttributeValueList: [{ S: connectionKey }]
+        },
+        sk: {
+          ComparisonOperator: "BEGINS_WITH",
+          AttributeValueList: [{ S: connectionPrefix }]
+        }
       }
-    }
-  }).promise();
+    })
+    .promise();
   // console.log(dynamoResponse.Items);
   if (dynamoResponse.Items === undefined) {
     return [];
   }
   return dynamoResponse.Items.map(item => {
-    const str = ((item.sk || {}).S || connectionPrefix);
-    return str.substr(connectionPrefix.length, str.length)
+    const str = (item.sk || {}).S || connectionPrefix;
+    return str.substr(connectionPrefix.length, str.length);
   });
-}
+};
